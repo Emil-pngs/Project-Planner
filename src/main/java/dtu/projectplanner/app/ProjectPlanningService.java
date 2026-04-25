f193c3bd3687046e5c7315bc6f176480e5691dec
mash <mashdtu@pm.me>
2026-04-25 13:44:50 +0200

    updated ProjectPlanningService to run the UI

--- src/main/java/dtu/projectplanner/app/ProjectPlanningService.java
+++ src/main/java/dtu/projectplanner/app/ProjectPlanningService.java
@@ -1,5 +1,12 @@
 package dtu.projectplanner.app;
 
+import dtu.projectplanner.ui.App;
+import javafx.application.Application;
+
 public class ProjectPlanningService {
-    
+
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module hellofx {
     requires transitive javafx.controls;
     requires javafx.fxml;
  
-    opens dtu.example.ui to javafx.fxml; // Gives access to fxml files
-    exports dtu.example.ui; // Exports the class inheriting from javafx.application.Application
+    opens dtu.projectplanner.ui to javafx.fxml; // Gives access to fxml files
+    exports dtu.projectplanner.ui; // Exports the class inheriting from javafx.application.Application
+    exports dtu.projectplanner.app; // Exports the main entry point
 }
